0c752d0b87daf70e4de9544fb0ab9322b03239f5
gdp12315 <gdp12315@163.com>
2017-01-06 19:59:13 +0800

    V1.0

---
+++ app/app.js
@@ -0,0 +1,29 @@
+/**
+ * app.js
+ *
+ * 整个应用的总入口
+ */
+
+// 让redux-saga支持es6语法包
+import 'babel-polyfill';
+
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+            	<h>Hello World</h>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>,document.getElementById('app'));
+
