d4e916bcdc8d91945725db300d7aa58828e2a4af
gdp12315 <gdp12315@163.com>
2017-01-09 22:04:49 +0800

    add folders

---
+++ app/containers/index.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+
+class Container extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+            	<h>Hello World</h>
+            </div>
+        );
+    }
+}
