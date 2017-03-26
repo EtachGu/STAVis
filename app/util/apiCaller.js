ab3b544e35c5d0627a16375d7cc791d17a085d2d
EtachGu <detached_gu@163.com>
2017-03-26 22:25:16 +0800

    fix CORS apiCaller

--- app/util/apiCaller.js
+++ app/util/apiCaller.js
@@ -6,6 +6,7 @@ const API_URL = Config.API_URL;
 export default function callApi(endpoint, method = 'get', body) {
   return fetch(`${API_URL}/${endpoint}`, {
     headers: { 'Content-Type': 'application/json' },
+	  mode: 'core',
     method,
     body: JSON.stringify(body),
   })
