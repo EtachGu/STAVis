0191e0f74793edb438e3945501a2189f2474c5f1
EtachGu <detached_gu@163.com>
2017-03-28 18:29:16 +0800

    add mapType to controls state

--- app/modules/TrajVA/TrajVAReducer.js
+++ app/modules/TrajVA/TrajVAReducer.js
@@ -7,7 +7,8 @@ const initialState = {
 	statistics:{},
 	controls:{
 	  	trajName:'cellPhoneTrack',
-	  	datetime:["2016-03-02","2016-03-03"]
+	  	datetime:["2016-03-02","2016-03-03"],
+	  	mapType: 1
   	},
 	tasks:{
 		steps:[{name:'概览',status:'finish'},{name:'分析',status:'process'},{name:'结论',status:'wait'},{name:'完成',status:'wait'}]
