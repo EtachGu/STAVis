dccb48edb98f6536fe0e83f15b08b215b1f0f5b2
EtachGu <detached_gu@163.com>
2017-04-28 19:27:49 +0800

    fix Indent

--- app/modules/TrajVA/components/GraphView/index.js
+++ app/modules/TrajVA/components/GraphView/index.js
@@ -13,32 +13,32 @@ import styles from './styles.css';
 
 class GraphView extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    const barWidth = 400;
-    const barHeight = 350;
+	render() {
+		const barWidth = 400;
+		const barHeight = 350;
 
-	const barData = this.props.statisticsData.length > 0 ? {
-		fields:this.props.statisticsData[0].fields,
-		data:this.props.statisticsData[0].data
-	} : undefined;
+		const barData = this.props.statisticsData.length > 0 ? {
+			fields:this.props.statisticsData[0].fields,
+			data:this.props.statisticsData[0].data
+		} : undefined;
 
-	const pieData = this.props.statisticsData.length > 0 ? {
-		legend:this.props.statisticsData[1].series,
-		fields:this.props.statisticsData[1].fields,
-		data:this.props.statisticsData[1].data
-	} :undefined;
+		const pieData = this.props.statisticsData.length > 0 ? {
+			legend:this.props.statisticsData[1].series,
+			fields:this.props.statisticsData[1].fields,
+			data:this.props.statisticsData[1].data
+		} :undefined;
 
-    return (
-      <div>
-        <BarCharts className={styles.charts} width={barWidth} height={barHeight} barData={barData}/>
-        <PieCharts className={styles.charts} width={barWidth} height={420} pieData={pieData}/>
-      </div>
-    );
-  }
+			return (
+				<div>
+					<BarCharts className={styles.charts} width={barWidth} height={barHeight} barData={barData}/>
+					<PieCharts className={styles.charts} width={barWidth} height={420} pieData={pieData}/>
+				</div>
+			);
+	}
 }
 
 GraphView.propTypes = {
@@ -48,14 +48,14 @@ GraphView.propTypes = {
 
 // 任何时候，只要 Redux store 发生改变，mapStateToProps 函数就会被调用。
 const mapStateToProps = createStructuredSelector({
-  statisticsData: selectStatistics
+	statisticsData: selectStatistics
 });
 
 // 如果你省略这个 mapDispatchToProps 参数，默认情况下，dispatch 会注入到你的组件 props 中。
 export function mapDispatchToProps(dispatch) {
-  return {
-    changeRoute: (url) => dispatch(push(url)),
-  };
+	return {
+		changeRoute: (url) => dispatch(push(url)),
+	};
 }
 
 // react-redux 的使用方式
