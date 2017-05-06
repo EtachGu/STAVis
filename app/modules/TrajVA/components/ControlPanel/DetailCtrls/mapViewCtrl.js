0e003e779ca9b2072cf5f801ed3d7a8189762f68
EtachGu <detached_gu@163.com>
2017-05-06 21:32:24 +0800

    add VisualMap

--- app/modules/TrajVA/components/ControlPanel/DetailCtrls/mapViewCtrl.js
+++ app/modules/TrajVA/components/ControlPanel/DetailCtrls/mapViewCtrl.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import echarts from 'echarts';
 // import components
 import {
 	Input,
@@ -27,6 +27,8 @@ const Option = Select.Option;
 import styles from '../styles.css';
 
 
+const COLOR_BAR = ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'];
+
 export class MapViewCtrl extends React.Component {
 	static propTypes = {
 		name: React.PropTypes.string,
@@ -42,15 +44,21 @@ export class MapViewCtrl extends React.Component {
 	}
 
 	showDetail = () => {
-
+		this.setState({
+			adSettingVisible: true
+		});
 	}
 
-	handlePopVisibleChange = () => {
-
+	handlePopVisibleChange = (visible) => {
+		this.setState({
+			adSettingVisible: visible
+		});
 	}
 
-	onVisualMapTypeChange = () => {
-
+	onVisualMapTypeChange = (e) => {
+		this.setState({
+			visualMapType: e.target.value
+		});
 	}
 
 	confirmSetting = () => {
@@ -68,6 +76,12 @@ export class MapViewCtrl extends React.Component {
 
   render() {
 
+  	const mapCharts = echarts.getInstanceByDom(document.getElementById('map'));
+
+  	const mapOption = mapCharts.getOption();
+
+  	const visualMapArray = mapOption.visualMap;
+
 	const advanceSetting = (
 		<div className={styles.advancsetting}>
 			<Row>
@@ -83,6 +97,28 @@ export class MapViewCtrl extends React.Component {
 						</RadioGroup>
 					</div>
 				</label>
+			</Row>
+			<Row>
+				<label>
+					主题
+					<div>
+						<RadioGroup
+								value={this.state.visualMapType}
+								onChange={this.onVisualMapTypeChange}
+							>
+								<Radio.Button value="continuous">连续型</Radio.Button>
+								<Radio.Button value="piecewise">分段型</Radio.Button>
+						</RadioGroup>
+					</div>
+				</label>
+			</Row>
+			<Row>
+				<label>
+					视觉系列列表
+				</label>
+				<div>
+					{[...visualMapArray.map((itemVM, index)=> (<span styel={{ float: 'left', backgroundColor: COLOR_BAR[ index / COLOR_BAR.length ], width: '20px', height: '20px', margin: '5px 5px' }} ></span>))]}
+				</div>
 			</Row>
 			<Button type="primary" onClick={this.confirmSetting}>确定</Button>
 			<Button type="primary" onClick={this.cancelSetting}>取消</Button>
@@ -110,20 +146,25 @@ export class MapViewCtrl extends React.Component {
 					 视觉映射
 					<Popover
 						placement="right"
-						title="设置"
+						title="视觉映射"
 						content={advanceSetting}
 						trigger="click"
 						visible={this.state.adSettingVisible}
 						onVisibleChange={this.handlePopVisibleChange}
 					>
 						<Button
-							style={{ float: "right", 'border-width': 0 }}
+							style={{ float: "right", borderWidth: 0 }}
 							size="small"
 							icon="caret-right"
 							onClick={this.showDetail}
 						/>
 					</Popover>
 				</label>
+			</Row>
+			<h4 style={{ marginTop: 10, marginBottom: 5 }}>系列几何设置</h4>
+			<hr style={{ marginTop: 5, marginBottom: 5 }} />
+			<Row>
+				
 			</Row>
 
 		</div>
